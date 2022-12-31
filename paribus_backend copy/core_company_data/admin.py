2dda4cbb1694a1f333f2835b942b494f8ba16e35
MatthewTe <teelucksingh.matthew1@gmail.com>
2022-12-31 13:35:36 -0800

    Removed copy

--- paribus_backend copy/core_company_data/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from core_company_data.models import CompanyCore
-
-admin.site.register(CompanyCore)
--- paribus_backend copy/core_company_data/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CoreCompanyDataConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'core_company_data'
