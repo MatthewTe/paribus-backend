4ee32ecaf8185d5c5fd84cc689d33c1ec5a7d80f
MatthewTe <teelucksingh.matthew1@gmail.com>
2022-12-31 15:17:57 -0800

    added new models for pricing data

--- paribus_backend/core_company_data/admin.py
+++ paribus_backend/core_company_data/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from core_company_data.models import CompanyCore
+from core_company_data.models import CompanyCore, PriceData
 
 admin.site.register(CompanyCore)
+admin.site.register(PriceData)
--- paribus_backend/core_company_data/models.py
+++ paribus_backend/core_company_data/models.py
@@ -1,6 +1,14 @@
 import uuid
 from django.db import models
 
+
+def default_ticker():
+    def inner(obj):
+        if obj.is_public:
+            return ''
+        return None
+    return inner
+
 class CompanyCore(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     company_name = models.CharField(max_length=100, blank=True)
@@ -8,7 +16,23 @@ class CompanyCore(models.Model):
     home_country = models.CharField(max_length=50)
     home_office = models.CharField(max_length=200)
     is_public = models.BooleanField(blank=True)
-    
+    ticker = models.CharField(max_length=10, blank=True, null=True)
+
     class Meta:
         verbose_name = "Core Company Metadata"
         verbose_name_plural = "Companies"
+
+
+class PriceData(models.Model):
+    ticker = models.CharField(max_length=10)
+    timestamp = models.DateTimeField()
+    open = models.FloatField()
+    high = models.FloatField()
+    low = models.FloatField()
+    close = models.FloatField()
+    volume = models.PositiveIntegerField()
+    market_cap = models.BigIntegerField()
+    company_id = models.ForeignKey(CompanyCore, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Company Price Timeseries"
